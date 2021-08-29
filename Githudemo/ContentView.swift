cc4941790c716d881d189f9af7331fabb82021c7
sunchuan88 <88229656+sunchuan88@users.noreply.github.com>
2021-08-29 16:50:29 +0800

    Initial Commit

---
+++ Githudemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Githudemo
+//
+//  Created by sun on 2021/8/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Githudemo/GithudemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithudemoApp.swift
+//  Githudemo
+//
+//  Created by sun on 2021/8/29.
+//
+
+import SwiftUI
+
+@main
+struct GithudemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
